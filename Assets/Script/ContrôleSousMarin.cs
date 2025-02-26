a9f1fa4674a1bf4e3e5de8f59b48e1412e0d69cc
Coulibaly Obadia <izied99ek3n@gmail.com>
2025-02-26 13:27:42 -0500

    7

--- Assets/Script/ContrôleSousMarin.cs
+++ Assets/Script/ContrôleSousMarin.cs
@@ -2,71 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class ContrôleSousMarin : MonoBehaviour
 {
-    
-    
-    // variables de mouvement et contrôle
-    [SerializeField] private float _vitesseDéplacement;
-    private Rigidbody _rb;
-    private Vector3 directionInput;
+    [SerializeField] private float _vitesseDéplacement = 5f;
+    private Rigidbody _rb; 
 
-    // variables de contrôle d'animation
-    [SerializeField] private float _modifierAnimTranslation;
+    private Vector2 directionInput;
     private Animator _animator;
-    private float _rotationVelocity;
 
-     void Start()
+    void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>(); 
+
         _animator = GetComponent<Animator>();
+
+        _animator = GetComponent<Animator>();
+
+        if (_rb == null)
+        {
+            Debug.LogError("Rigidbody manquant sur le sous-marin !");
+        }
     }
 
     void OnDéplacer(InputValue directionBase)
     {
-        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitesseDéplacement;
-        directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
+        directionInput = directionBase.Get<Vector2>() * _vitesseDéplacement;
     }
 
-   
-
-     void FixedUpdate()
+    void FixedUpdate()
     {
-        // calculer et appliquer la translation
-        Vector3 mouvement = directionInput;
-        float rotation = 0f;
-        // si on a une direction d'input
-        if (directionInput.magnitude > 0f)
-        {
-            // calculer rotation cible
-            float rotationCible = Vector3.SignedAngle(-Vector3.forward, directionInput, Vector3.up);
-            // faire le changement plus graduel avec une interpolation
-            rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationCible, ref _rotationVelocity, 0.12f);
-            // appliquer la rotation cible directement
-            _rb.MoveRotation(Quaternion.Euler(0.0f, rotation, 0.0f));
-        }
-        // appliquer la vitesse de translation
-        _rb.AddForce(mouvement, ForceMode.VelocityChange);
+        _rb.velocity = directionInput;
 
-        // calculer un modifiant pour la vitesse d'animation
-        Vector3 vitesseSurPlane = new Vector3(_rb.velocity.x, 0f, _rb.velocity.y);
-        _animator.SetFloat("Vitesse", vitesseSurPlane.magnitude * _modifierAnimTranslation);
-        _animator.SetFloat("Déplacement", vitesseSurPlane.magnitude);
+        // Animation
+        _animator.SetFloat("Vitesse", directionInput.magnitude);
     }
 
     void OnMonterDescendre(InputAction.CallbackContext context)
     {
-        _monterDescendreInput = context.ReadValue<float>();
-        float InputValue = context.ReadValue<float>();
-        directionInput.y = InputValue * _vitesseDéplacement;
+        float inputValue = context.ReadValue<float>();
+        directionInput.y = inputValue * _vitesseDéplacement;
     }
 
     void OnAccelerer(InputAction.CallbackContext context)
     {
-        _monterDescendreInput = context.ReadValue<float>();
-        float InputValue = context.ReadValue<float>();
-        directionInput.y = InputValue * _vitesseDéplacement;
+        float inputValue = context.ReadValue<float>();
+        directionInput.x = inputValue * _vitesseDéplacement;
     }
 }
